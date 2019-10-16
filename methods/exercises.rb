d9c3e560c6192668f9d3eca5d439c67162bef70b
Garry Ledwith <garryledwith@gmail.com>
2019-10-16 06:16:04 +0100

    Exercise complete

---
+++ methods/exercises.rb
@@ -0,0 +1,46 @@
+# Exercise 1
+
+# def greeting(name)
+#   "Hello #{name}"
+# end
+#
+# puts greeting("Garry")
+
+# Exercise 2
+#
+# 1. x = 2  # => 2
+#
+# 2. puts x = 2 # => nil
+#
+# 3. p name = "Garry" # => "Garry"
+#
+# 4. four = "four" # => "four"
+#
+# 5. print something = "nothing" # => nil
+
+# Exercise 3
+
+ def multiply(num1, num2)
+  num1 * num2
+ end
+
+puts multiply(4, 4)
+
+# Exercise 4
+
+def scream(words)
+  words = words + "!!!!"
+  return
+  puts words
+end
+
+scream("Yippeee")  # => Will print nothing to the screen
+
+# Exercise 5
+
+def scream(words)
+  words = words + "!!!!"
+  puts words
+end
+
+scream("Yippeee") => nil 
---
+++ methods/say.rb
@@ -0,0 +1,23 @@
+# puts "hello"
+# puts "hi"
+# puts "how are you"
+# puts "I'm fine"
+#
+# def say(words='hello')
+#   puts words + '.'
+# end
+#
+#
+# say("hello")
+# say("hi")
+# say("how are you")
+# say("I''m fine")
+# say()
+
+def some_method(numner)
+  number = 7
+end
+
+a = 5
+some_method(a)
+puts a
